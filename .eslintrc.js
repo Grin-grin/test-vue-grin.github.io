cfcdf3ad1a938499202b5efb1101846908315cbc
grin <g.vasiliev@runetsoft.ru>
2024-03-14 20:06:11 +0300

    initial

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,27 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
+    es2021: true,
     node: true,
+    "vue/setup-compiler-macros": true
   },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    "plugin:prettier/recommended"
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2021
   },
+  ignorePatterns: ["/build", "/assets"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    "prefer-const": ["off"],
+    "vue/multi-word-component-names": ["off"],
+    "@typescript-eslint/no-namespace": ["off"],
+    "@typescript-eslint/no-empty-interface": ["off"]
+  }
 };
--- README.md
+++ README.md
@@ -1,24 +1,31 @@
-# test-tages-2
+# test-vue
 
 ## Project setup
+
 ```
 npm install
 ```
 
+### Node v18.12.1
+
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
